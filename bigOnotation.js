6cc2bc3ebe9fc9f4b514d4ae2f300663184a1186
Carlo091103 <161405370+Carlo091103@users.noreply.github.com>
2024-12-19 12:15:44 +0800

    Create bigOnotation.js

---
+++ bigOnotation.js
@@ -0,0 +1,23 @@
+const productList = ["Shoes", "Shirt", "Hat", "Bag"];
+
+const findProductPairs = (products) => {
+    const pairs = [];
+    for (let i = 0; i < products.length; i++) {
+        for (let j = i + 1; j < products.length; j++) {
+            pairs.push(${products[i]} and ${products[j]});
+        }
+    }
+    return pairs;
+};
+
+console.log(findProductPairs(productList));
+module.exports = { findProductPairs };
+
+[
+  'Shoes and Shirt',
+  'Shoes and Hat',
+  'Shoes and Bag',
+  'Shirt and Hat',
+  'Shirt and Bag',
+  'Hat and Bag'
+]
